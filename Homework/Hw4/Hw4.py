8281290d9b00eacbc78eed3f2aa55fa98b7b8804
kevin-quizhpi <kevin.quizhpi@live.com>
2017-10-23 03:14:36 -0400

    Prob 4 left

--- Homework/Hw4/Hw4.py
+++ Homework/Hw4/Hw4.py
@@ -6,7 +6,6 @@ windowName = "Bean";
 h = np.array([1, 4, 6, 4, 1],float);
 sum = np.sum(h)
 h = h/sum
-print h
 
 def display(window, im):
 	while True:
@@ -14,71 +13,51 @@ def display(window, im):
 		cv2.imshow(window, im)
 		key = cv2.waitKey(1) & 0xFF
 		# if the 'c' key is pressed, break from the loop
-		if key == ord("c"):
+		if key == ord("n"):
 			break
 	cv2.destroyAllWindows()
+
+def getGaussian(im, fil):
+	height, width = im.shape[:2]
+	temp = np.zeros((height, width,3), np.uint8)
+	cv2.filter2D(im, -1, fil, temp)
+	temp =cv2.resize(temp,(height/2,width/2))
+
+	return temp
+
+def getLaplacian(im, fil):
+	height, width = im.shape[:2]
+	temp = np.zeros((height/2,width/2,3), np.uint8)
+	temp = getGaussian(im, fil)
+	im = cv2.resize(im,(height/2,width/2))
+	Lap = im - temp
+
+	return Lap, temp
+
 
 
 def main():
 	image = cv2.imread('Bean.jpg',1);
+	G = cv2.resize(image,(500,500))
 	image = cv2.resize(image,(500,500))
 	display(windowName, image)
+	tmp = np.zeros((500, 500, 3), np.uint8)
 
+	# Problem 1: 4 levels of a guassian pyramid
+	# Press N to cycle through images
+	for x in range(1,5):
+		G = getGaussian(G,h)
+		display('Guassian', G)
 
-	height, width = image.shape[:2]
-	G1 = np.zeros((height,width,3), np.uint8);
-	#cv2.sepFilter2D(image,-1,h,h,G1)
+		
+	# Problem 2: 4 levels of a laplacian pyramid
+	# Press N to cycle through images
+	tmp = np.zeros((500/2, 500/2, 3), np.uint8)
+	G = cv2.resize(image,(500,500))
+	for x in range(1,5):
+		L, G = getLaplacian(G,h)
+		display('laplacian', L)
 
-	cv2.filter2D(image,-1,h, G1)
-	
-	# #cv2.pyrDown(G1,G1,(height/2,width/2))
-	display('Gaussian 1',G1)
-	L1 =  image - G1
-	display('Laplacian1', L1)
-	G1 = cv2.resize(G1,(height/2,width/2))
-	image = cv2.resize(image,(height/2,width/2))
-	width, height = G1.shape[:2]
-	G2 = np.zeros((height,width,3), np.uint8);
-	cv2.filter2D(G1,-1,h, G2)
-	
-	#cv2.pyrDown(G2,G2,(height/2,width/2))
-	display('Gaus2sian ',G2)
-	L2 = G1 - G2
-	display('Laplacian', L2)
-	G2 = cv2.resize(G2,(height/2,width/2))
-	G1 = cv2.resize(G1,(height/2,width/2))
-	width, height = G2.shape[:2]
-	G3 = np.zeros((height,width,3), np.uint8);
-	cv2.filter2D(G2,-1,h, G3)
-	
-	#cv2.pyrDown(G3,G3,(height/2,width/2))
-	display('Gaussian 2',G3)
-	L3 = G2 - G3
-	display('Laplacian2', L3)
-	G3 = cv2.resize(G3,(height/2,width/2))
-	G2 = cv2.resize(G2,(height/2,width/2))
-	width, height = G3.shape[:2]
-	#G3 = cv2.resize(G3,(500,500))
-	#L3 = cv2.resize(L3,(500,500))
-
-	G4 = np.zeros((height,width,3), np.uint8);
-	cv2.filter2D(G3, -1,h,G4)
-	L4 = G3 - G4
-	display('G4',G4)
-	display('L4',L4)
-
-
-	# G5 = np.zeros((height,width,3), np.uint8);
-	# cv2.filter2D(G4, -1,h,G5)
-	# L5 = G5 - G4
-	# display('G5',G5)
-	# display('L5',L5)
-
-	# G6 = np.zeros((height,width,3), np.uint8);
-	# cv2.filter2D(G5, -1,h,G6)
-	# L6 = G6 - G5
-	# display('G6',G6)
-	# display('L6',L6)
 
 
 
